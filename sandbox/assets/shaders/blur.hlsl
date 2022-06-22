6c2fe18b3ce1a7cfee04ca42f005c483ad0ea50e
Gleb <navka.gleb@gmail.com>
2022-06-22 18:02:43 +0300

    :recycle: Refactor Blur shader
    1. MAX_BLUR_RADIUS move to const variable because there is no way to transfer data to array throw 32bit constants to shader. So Weight0 - Weight are left in Settings constant bufer
    2. Replace Texture.Length to Texture.GetDimensions()

--- sandbox/assets/shaders/blur.hlsl
+++ sandbox/assets/shaders/blur.hlsl
@@ -2,26 +2,24 @@
     #define THREAD_PER_GROUP_COUNT 256
 #endif
 
-#if !defined(MAX_BLUR_RADIUS)
-    #define MAX_BLUR_RADIUS 5
-#endif
+static const int g_MaxBlurRadius = 5;
 
 struct Settings
 {
     int BlurRadius;
-
-    // Support up to 11 blur weights
-    float W0;
-    float W1;
-    float W2;
-    float W3;
-    float W4;
-    float W5;
-    float W6;
-    float W7;
-    float W8;
-    float W9;
-    float W10;
+    
+    // Support up to 11 blur weight
+    float Weight0;
+    float Weight1;
+    float Weight2;
+    float Weight3;
+    float Weight4;
+    float Weight5;
+    float Weight6;
+    float Weight7;
+    float Weight8;
+    float Weight9;
+    float Weight10;
 };
 
 ConstantBuffer<Settings> g_Settings : register(b0);
@@ -29,7 +27,7 @@ ConstantBuffer<Settings> g_Settings : register(b0);
 Texture2D g_Input : register(t0);
 RWTexture2D<float4> g_Output : register(u0);
 
-groupshared float4 g_Cache[THREAD_PER_GROUP_COUNT + 2 * MAX_BLUR_RADIUS];
+groupshared float4 g_Cache[THREAD_PER_GROUP_COUNT + 2 * g_MaxBlurRadius];
 
 struct CS_Input
 {
@@ -42,19 +40,23 @@ void CS_HorizontalBlur(CS_Input input)
 {
     const float weights[11] =
     { 
-        g_Settings.W0,
-        g_Settings.W1,
-        g_Settings.W2,
-        g_Settings.W3,
-        g_Settings.W4,
-        g_Settings.W5,
-        g_Settings.W6,
-        g_Settings.W7,
-        g_Settings.W8,
-        g_Settings.W9,
-        g_Settings.W10
+        g_Settings.Weight0,
+        g_Settings.Weight1,
+        g_Settings.Weight2,
+        g_Settings.Weight3,
+        g_Settings.Weight4,
+        g_Settings.Weight5,
+        g_Settings.Weight6,
+        g_Settings.Weight7,
+        g_Settings.Weight8,
+        g_Settings.Weight9,
+        g_Settings.Weight10
     };
-
+    
+    float inputWidth;
+    float inputHeight;
+    g_Input.GetDimensions(inputWidth, inputHeight);
+    
     if (input.GroupThreadID.x < g_Settings.BlurRadius)
     {
         const int inputX = max(input.DispatchThreadID.x - g_Settings.BlurRadius, 0);
@@ -65,13 +67,13 @@ void CS_HorizontalBlur(CS_Input input)
     
     if (input.GroupThreadID.x >= THREAD_PER_GROUP_COUNT - g_Settings.BlurRadius)
     {
-        const int inputX = min(input.DispatchThreadID.x + g_Settings.BlurRadius, g_Input.Length.x - 1);
+        const int inputX = min(input.DispatchThreadID.x + g_Settings.BlurRadius, inputWidth - 1);
         const int inputY = input.DispatchThreadID.y;
         
         g_Cache[input.GroupThreadID.x + 2 * g_Settings.BlurRadius] = g_Input[int2(inputX, inputY)];
     }
 
-    g_Cache[input.GroupThreadID.x + g_Settings.BlurRadius] = g_Input[min(input.DispatchThreadID.xy, g_Input.Length.xy - 1)];
+    g_Cache[input.GroupThreadID.x + g_Settings.BlurRadius] = g_Input[min(input.DispatchThreadID.xy, int2(inputWidth, inputHeight) - 1)];
 
     GroupMemoryBarrierWithGroupSync();
 
@@ -93,19 +95,23 @@ void CS_VerticalBlur(CS_Input input)
 {
     const float weights[11] =
     {
-        g_Settings.W0,
-        g_Settings.W1,
-        g_Settings.W2,
-        g_Settings.W3,
-        g_Settings.W4,
-        g_Settings.W5,
-        g_Settings.W6,
-        g_Settings.W7,
-        g_Settings.W8,
-        g_Settings.W9,
-        g_Settings.W10
+        g_Settings.Weight0,
+        g_Settings.Weight1,
+        g_Settings.Weight2,
+        g_Settings.Weight3,
+        g_Settings.Weight4,
+        g_Settings.Weight5,
+        g_Settings.Weight6,
+        g_Settings.Weight7,
+        g_Settings.Weight8,
+        g_Settings.Weight9,
+        g_Settings.Weight10
     };
 
+    float inputWidth;
+    float inputHeight;
+    g_Input.GetDimensions(inputWidth, inputHeight);
+    
     if (input.GroupThreadID.y < g_Settings.BlurRadius)
     {
         const int inputX = input.DispatchThreadID.x;
@@ -117,18 +123,18 @@ void CS_VerticalBlur(CS_Input input)
     if (input.GroupThreadID.y >= THREAD_PER_GROUP_COUNT - g_Settings.BlurRadius)
     {
         const int inputX = input.DispatchThreadID.x;
-        const int inputY = min(input.DispatchThreadID.y + g_Settings.BlurRadius, g_Input.Length.y - 1);
+        const int inputY = min(input.DispatchThreadID.y + g_Settings.BlurRadius, inputHeight - 1);
         
         g_Cache[input.GroupThreadID.y + 2 * g_Settings.BlurRadius] = g_Input[int2(inputX, inputY)];
     }
 
-    g_Cache[input.GroupThreadID.y + g_Settings.BlurRadius] = g_Input[min(input.DispatchThreadID.xy, g_Input.Length.xy - 1)];
+    g_Cache[input.GroupThreadID.y + g_Settings.BlurRadius] = g_Input[min(input.DispatchThreadID.xy, int2(inputWidth, inputHeight) - 1)];
 
     GroupMemoryBarrierWithGroupSync();
 
     float4 blurColor = float4(0, 0, 0, 0);
 
-    for (int i = -g_Settings.BlurRadius; i <= g_Settings.BlurRadius; ++i)
+    for (int i = -g_Settings.BlurRadius; i < g_Settings.BlurRadius + 1; ++i)
     {
         const int weightIndex = i + g_Settings.BlurRadius;
         const int cacheIndex = input.GroupThreadID.y + g_Settings.BlurRadius + i;
