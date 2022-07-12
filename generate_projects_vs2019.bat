ba262ae6b7b2c994928f4b41ad7d27c539d1e648
navkagleb <navka.gleb@gmail.com>
2022-07-12 16:26:41 +0300

    :white_check_mark: Embed the premake
    1. Add spieler.lua config file for premake
    2. Add premake5.exe to generate necessary files
    3. Remove Visual Studio generated files
    
    Note: for now Release and Final configurations are disable

---
+++ generate_projects_vs2019.bat
@@ -0,0 +1,2 @@
+call tools\premake5.exe --file=tools\spieler.lua vs2019
+PAUSE
---
+++ tools/spieler.lua
@@ -0,0 +1,130 @@
+workspace "spieler"
+    location "../"
+
+    platforms { "Win64" }
+
+    configurations
+    {
+        "Debug",
+        -- "Release",
+        -- "Final"
+    }
+
+    startproject "sandbox"
+
+    filter "platforms:Win64"
+        system "windows"
+        systemversion "latest"
+        architecture "x64"
+        characterset "ASCII"
+        linkoptions { "/ENTRY:mainCRTStartup" }
+
+        defines { "SPIELER_WIN64", "WIN32" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG", "SPIELER_DEBUG" }
+        optimize "Off"
+
+--[[
+    filter "configurations:Release"
+        defines { "NDEBUG", "SPIELER_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Final"
+        defines { "NDEBUG", "SPIELER_FINAL" }
+        optimize "On"
+
+    filter { "files:**.hlsl" }
+		buildaction "None"
+--]]
+
+
+outputdir = "%{cfg.buildcfg}/%{cfg.system}/%{cfg.architecture}"
+
+project "third_party"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    location "../third_party"
+
+    targetdir ("../bin/" ..outputdir)
+    objdir ("../bin/int/" .. outputdir)
+
+    files
+    {
+        -- directx
+        "../third_party/directx/**.h",
+        "../third_party/directx/**.cpp",
+
+        --fmt
+        "../third_party/fmt/**.h",
+        "../third_party/fmt/**.cc",
+
+        -- imgui
+        "../third_party/imgui/**.h",
+        "../third_party/imgui/**.cpp",
+
+        -- magic_enum
+        "../third_party/magic_enum/*.hpp"
+    }
+
+project "spieler"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    location "../spieler"
+
+    targetdir ("../bin/" .. outputdir)
+    objdir ("../bin/int/" .. outputdir)
+
+    pchheader "spieler/config/bootstrap.hpp"
+    pchsource "../spieler/src/config/bootstrap.cpp"
+
+    files
+    {
+        "../spieler/include/spieler/**.hpp",
+        "../spieler/include/spieler/**.inl",
+        "../spieler/src/**.hpp",
+
+        "../spieler/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "../",
+        "../spieler/include",
+        "../spieler/src"
+    }
+
+project "sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    location "../sandbox"
+
+    targetdir ("../bin/" .. outputdir)
+    objdir ("../bin/int/" .. outputdir)
+
+    links
+    {
+        "third_party",
+        "spieler"
+    }
+
+    pchheader "bootstrap.hpp"
+    pchsource "../sandbox/src/bootstrap.cpp"
+
+    files
+    {
+        "../sandbox/src/**.hpp",
+        "../sandbox/src/**.inl",
+
+        "../sandbox/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "../",
+        "../spieler/include",
+        "../sandbox/src"
+    }
