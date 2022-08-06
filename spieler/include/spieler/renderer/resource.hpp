d0bb5e19d60987add73db06e8d342201b4cc64be
Hlib Navka <navka.gleb@gmail.com>
2022-08-06 11:04:57 +0300

    :white_check_mark: Add Move ctor to Resource and Release method

--- spieler/include/spieler/renderer/resource.hpp
+++ spieler/include/spieler/renderer/resource.hpp
@@ -20,13 +20,19 @@ namespace spieler::renderer
     public:
         Resource() = default;
         Resource(ComPtr<ID3D12Resource>&& dx12Resource);
+        Resource(Resource&& other) noexcept;
 
     public:
         ID3D12Resource* GetDX12Resource() const { return m_DX12Resource.Get(); }
 
     public:
         GPUVirtualAddress GetGPUVirtualAddress() const { return static_cast<GPUVirtualAddress>(m_DX12Resource->GetGPUVirtualAddress()); }
+
+        void Release();
         
+    public:
+        Resource& operator=(Resource&& other) noexcept;
+
     protected:
         ComPtr<ID3D12Resource> m_DX12Resource;
     };
--- spieler/src/renderer/resource.cpp
+++ spieler/src/renderer/resource.cpp
@@ -9,4 +9,25 @@ namespace spieler::renderer
         : m_DX12Resource{ std::exchange(dx12Resource, nullptr) }
     {}
 
+    Resource::Resource(Resource&& other) noexcept
+        : m_DX12Resource{ std::exchange(other.m_DX12Resource, nullptr) }
+    {}
+
+    void Resource::Release()
+    {
+        m_DX12Resource.Reset();
+    }
+
+    Resource& Resource::operator=(Resource&& other) noexcept
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+
+        m_DX12Resource.Swap(std::move(other.m_DX12Resource));
+
+        return *this;
+    }
+
 } // namespace spieler::renderer
